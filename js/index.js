cdcd807dbb96f9d34a7c68eae01189544a4aaaee
sw-li <lishengwei90@gmail.com>
2022-11-16 21:29:42 +0100

    all works, to be checked in the runner

--- js/index.js
+++ js/index.js
@@ -3,12 +3,12 @@
 function updateSubtotal(product) {
   //single input calculation test for iteration 1
   //console.log('Calculating subtotal, yey!')
-  const price = product.children.item(1)
+  if(product != null)
+ { const price = product.children.item(1)
   const quantity = product.children.item(2)
   const subtotal = product.children.item(3)
   subtotal.innerText = parseFloat(price.firstElementChild.innerText) * parseFloat(quantity.firstElementChild.value)
-  return subtotal.innerText
-
+  return subtotal.innerText}
 /*   const price = parseFloat(document.querySelector(".price span").innerText)
   const quantity = parseFloat(document.querySelector(".quantity input").value)
   const subtotal = document.querySelector(".subtotal span")
@@ -30,10 +30,14 @@ function calculateAll() {
   const products = document.getElementsByClassName("product")
   for(let i in products){updateSubtotal(products.item(i))}
   const subtotals = document.getElementsByClassName("subtotal")
+  console.log(subtotals)
   //subtotals is a HTMLcollection element, not an array, we can't use reduce() here
   let sum = 0
-  for(let i in subtotals) sum+= parseFloat(subtotals.item(i).lastChild.textContent)
-
+  if(subtotals.length != 0){
+    for(let i in subtotals){
+      sum+= parseFloat(subtotals.item(i).lastChild.textContent)
+    }
+  }
   // ITERATION 3
   const total = document.getElementById('total-value').firstElementChild
   total.innerText = sum
@@ -58,15 +62,48 @@ function createProduct(event) {
  const newProductPrice = document.getElementById('new-product-price').value
  // deep clone the first product
  const firstProduct = document.querySelector('.product')
- const newProduct = firstProduct.cloneNode(true)
+ /* const newProduct = firstProduct.cloneNode(true) */
+ const newProduct = newTr()
  console.log(newProduct)
  newProduct.children.item(0).firstElementChild.innerText = newProductName
  newProduct.children.item(1).firstElementChild.innerText = newProductPrice
-
+ newProduct.children.item(4).firstElementChild.addEventListener('click', removeProduct)
  const parent= document.querySelector("tbody")
  parent.appendChild(newProduct) 
-
 }
+
+function newTr(){
+  const newProduct = document.createElement("tr")
+  newProduct.className = "product"
+  const name = newProduct.appendChild(document.createElement("td"))
+  const price = newProduct.appendChild(document.createElement("td"))
+  const quantity = newProduct.appendChild(document.createElement("td"))
+  const subtotal = newProduct.appendChild(document.createElement("td"))
+  const action = newProduct.appendChild(document.createElement("td"))
+  name.className = "name"
+  name.appendChild(document.createElement("span"))
+  name.firstElementChild.innerText = "new product"
+  price.className = "price"
+  price.innerHTML = "$"
+  price.appendChild(document.createElement("span"))
+  price.firstElementChild.innerText = "20.00"
+  quantity.className = "quantity"
+  quantity.appendChild(document.createElement("input"))
+  quantity.firstElementChild.type = "number"
+  quantity.firstElementChild.value = "0"
+  quantity.firstElementChild.min = "0"
+  quantity.firstElementChild.Placeholder = "Quantity"
+  subtotal.className = "subtotal"
+  subtotal.innerHTML="$"
+  subtotal.appendChild(document.createElement("span"))
+  action.className = "action"
+  action.appendChild(document.createElement("button"))
+  action.firstElementChild.className = "btn btn-remove"
+  action.firstElementChild.innerHTML = "Remove"
+  return newProduct
+}
+
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
